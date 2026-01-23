7f0f70a5eb65d877629e7b85392f90f548436ca7
bebiksior <bebiks.priv@gmail.com>
2026-01-23 23:14:48 +0100

    Remove share match & replace functionality (now in Caido core)

--- caido.config.ts
+++ caido.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   id,
   name: "EvenBetter",
   description: "Collection of tweaks and improvements for Caido",
-  version: "4.0.1",
+  version: "4.0.2",
   author: {
     name: "bebiks",
     email: "bebiks@cvssadvisor.com",
